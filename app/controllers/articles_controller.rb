d0b8de4a7482b691b42fc4c3e9ebf498c03ca9b9
Mfbeeck <mfbeeck@gmail.com>
2015-07-21 15:31:54 -0400

    linked up delete with show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,36 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
 
 	def show
 		@article = Article.find(params[:id])
+
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		# @article = Article.new(title: params[:article][:title],body: params[:article][:body])
+ 		# or
+ 		# @article.title = params[:article][:title]
+ 		# @article.body = params[:article][:body]
+ 		# This isn't best because it's repetitive. You could do then:
+ 		# @article = Article.new(params[:article]) BUT THIS DOESNT WORK BC IT IS DANGEROUS TO JUST ACCEPT ANY PARAMS GIVEN, WE MUST SEPECIFY LIKE BELOW:
+  		@article = Article.new(article_params) #look at new method, article_params in the helper file for articles to understand
+
+  		@article.save
+  		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
 
 	end
 end
