5cea4b2396ac300fdf8b8f34b94af90e940223c7
Mfbeeck <mfbeeck@gmail.com>
2015-07-22 11:22:10 -0400

    added paperclip image uploading

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
 	def article_params
-  		params.require(:article).permit(:title, :body, :created_at, :updated_at, :tag_list)
+  		params.require(:article).permit(:title, :body, :created_at, :updated_at, :tag_list, :image)
 	end
 end
